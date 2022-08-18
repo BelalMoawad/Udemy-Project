fa59cdce2af8fdf8bbc47eb7b319e6018910e4c6
BelalMoawad <40965840+BelalMoawad@users.noreply.github.com>
2022-08-18 21:16:56 +0200

    BLD-3-sliding

--- scripts.js
+++ scripts.js
@@ -1,34 +1,77 @@
-let pythonCourses;
+var swiper = new Swiper(".CP", 
+{
+  watchoverflow : true,
+  slidesPerView: 5,
+  spaceBetween: 30,
+  slidesPerGroup: 4,
+  loop: false,
+  loopFillGroupWithBlank: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    0: {
+      slidesPerView : 1,
+      slidesPerGroup : 1
+    },
+    550: {
+      slidesPerView : 2,
+      slidesPerGroup : 1
+    },
+    750: {
+      slidesPerView : 3,
+      slidesPerGroup : 2
+    },
+    1000: {
+      slidesPerView : 4,
+      slidesPerGroup : 3
+    },
+    1200: {
+      slidesPerView : 5,
+      slidesPerGroup : 4
+    }
+  }
+});
 
-fetch("http://myjson.dit.upm.es/api/bins/95hg")
+let allCourses;
+
+let filter = "Pythoncourses";
+
+fetch("http://myjson.dit.upm.es/api/bins/789s")
   .then((response) => response.json())
   // get json of python cources ok
   .then((json) => {
-    pythonCourses = json;
-    for(let courceIdx = 0; courceIdx < pythonCourses.length; courceIdx++) {
+    allCourses = json;
+    let course = allCourses[filter];
+    for(let courceIdx = 0; courceIdx < course.length; courceIdx++) {
         // create new div and give it a class
         let newDiv = document.createElement("div");
-        newDiv.className = "pythonCard";
+        newDiv.classList.add("pythonCard", "swiper-slide");
         // get an image of python course
         let courseImage = document.createElement("img");
         let a = document.createElement("a");
         // Author creation, give class, give content 
         let Author_span = document.createElement("span");
         Author_span.className = "Author";
-        let Author_content = document.createTextNode(pythonCourses[courceIdx].author);
+        let Author_content = document.createTextNode(course[courceIdx].author);
         Author_span.appendChild(Author_content);
         // Price creation, give class, give content 
         let Price_span = document.createElement("span");
         Price_span.className = "Price";
         Price_span.innerHTML = "&#69;&#163;";
-        let Price_content = document.createTextNode(pythonCourses[courceIdx].price);
+        let Price_content = document.createTextNode(course[courceIdx].price);
         Price_span.appendChild(Price_content);
         // get source of courseImg
-        courseImage.src = pythonCourses[courceIdx].image;
+        courseImage.src = course[courceIdx].image;
         // title content
-        let titleContent = document.createTextNode(pythonCourses[courceIdx].title);
+        let titleContent = document.createTextNode(course[courceIdx].title);
         // a link 
-        a.href = pythonCourses[courceIdx].link;
+        a.href = course[courceIdx].link;
         a.appendChild(courseImage);
         a.appendChild(titleContent);
         a.appendChild(Author_span);
@@ -39,37 +82,81 @@ fetch("http://myjson.dit.upm.es/api/bins/95hg")
     }
   });
 
-  document.querySelector(".search-bar input").addEventListener("input", (event) => {
+
+function printList(course_name) 
+{
+
+  let pythonCards = document.querySelector(".pythonCards");
+  pythonCards.innerHTML = "";
+  filter = course_name;
+  let course = allCourses[filter];
+    for(let courceIdx = 0; courceIdx < course.length; courceIdx++) 
+    {
+        // create new div and give it a class
+        let newDiv = document.createElement("div");
+        newDiv.classList.add("pythonCard", "swiper-slide");
+        // get an image of python course
+        let courseImage = document.createElement("img");
+        let a = document.createElement("a");
+        // Author creation, give class, give content 
+        let Author_span = document.createElement("span");
+        Author_span.className = "Author";
+        let Author_content = document.createTextNode(course[courceIdx].author);
+        Author_span.appendChild(Author_content);
+        // Price creation, give class, give content 
+        let Price_span = document.createElement("span");
+        Price_span.className = "Price";
+        Price_span.innerHTML = "&#69;&#163;";
+        let Price_content = document.createTextNode(course[courceIdx].price);
+        Price_span.appendChild(Price_content);
+        // get source of courseImg
+        courseImage.src = course[courceIdx].image;
+        // title content
+        let titleContent = document.createTextNode(course[courceIdx].title);
+        // a link 
+        a.href = course[courceIdx].link;
+        a.appendChild(courseImage);
+        a.appendChild(titleContent);
+        a.appendChild(Author_span);
+        a.appendChild(Price_span);
+        // appent link to newDiv
+        newDiv.append(a);
+        document.querySelector(".pythonCards").appendChild(newDiv);
+    }
+}
+
+document.querySelector(".search-bar input").addEventListener("input", (event) => {
         let pythonCards = document.querySelector(".pythonCards");
         pythonCards.innerHTML = "";
-        for(let courceIdx = 0; courceIdx < pythonCourses.length; courceIdx++) {
-              let Mytitle = pythonCourses[courceIdx].title.toLowerCase();
+        let course = allCourses[filter];
+        for(let courceIdx = 0; courceIdx < course.length; courceIdx++) {
+              let Mytitle = course[courceIdx].title.toLowerCase();
               let searchPart = event.target.value.toLowerCase();
               if(Mytitle.includes(searchPart) === false) 
                   continue;
               // create new div and give it a class
               let newDiv = document.createElement("div");
-              newDiv.className = "pythonCard";
+              newDiv.classList.add("pythonCard", "swiper-slide");
               // get an image of python course
               let courseImage = document.createElement("img");
               let a = document.createElement("a");
               // Author creation, give class, give content 
               let Author_span = document.createElement("span");
               Author_span.className = "Author";
-              let Author_content = document.createTextNode(pythonCourses[courceIdx].author);
+              let Author_content = document.createTextNode(course[courceIdx].author);
               Author_span.appendChild(Author_content);
               // Price creation, give class, give content 
               let Price_span = document.createElement("span");
               Price_span.className = "Price";
               Price_span.innerHTML = "&#69;&#163;";
-              let Price_content = document.createTextNode(pythonCourses[courceIdx].price);
+              let Price_content = document.createTextNode(course[courceIdx].price);
               Price_span.appendChild(Price_content);
               // get source of courseImg
-              courseImage.src = pythonCourses[courceIdx].image;
+              courseImage.src = course[courceIdx].image;
               // title content
-              let titleContent = document.createTextNode(pythonCourses[courceIdx].title);
+              let titleContent = document.createTextNode(course[courceIdx].title);
               // a link 
-              a.href = pythonCourses[courceIdx].link;
+              a.href = course[courceIdx].link;
               a.appendChild(courseImage);
               a.appendChild(titleContent);
               a.appendChild(Author_span);
@@ -78,5 +165,4 @@ fetch("http://myjson.dit.upm.es/api/bins/95hg")
               newDiv.append(a);
               pythonCards.appendChild(newDiv);
         }
-  });
-
+});
